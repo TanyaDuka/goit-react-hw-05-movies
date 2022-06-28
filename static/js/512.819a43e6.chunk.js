8368bc3df362ea6e15f5a2ff31ebcf070954b6dc
TanyaDuka <70702007+TanyaDuka@users.noreply.github.com>
2022-06-28 10:25:30 +0000

    Deploying to gh-pages from @ TanyaDuka/goit-react-hw-05-movies@ec3d80104add6f26b4bd7cbd95d7352bb87933ff 🚀

---
+++ static/js/512.819a43e6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[512],{5512:function(e,n,t){t.r(n),t.d(n,{default:function(){return i}});var u=t(184);function i(){return(0,u.jsx)("h1",{children:"Nothing found"})}}}]);
+//# sourceMappingURL=512.819a43e6.chunk.js.map
